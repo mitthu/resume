614439fd288a26e8b3d754804b70d0c0c80894ac
Aditya Basu <ab.aditya.basu@gmail.com>
2019-08-23 14:57:05 -0400

    build: put quotes around $PATH
    - This is necessary when there are spaces in the existing PATH.

--- build/texlive.sh
+++ build/texlive.sh
@@ -9,7 +9,7 @@
 # Taken on: July 8, 2019
 
 # See if there is a cached version of TL available
-export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+export PATH="/tmp/texlive/bin/x86_64-linux:$PATH"
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
   wget -nv http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
